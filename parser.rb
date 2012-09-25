e6fa9194895ac72bfed3918c0700a13b9d3ba5b1
John J Wang <wangjohn@mit.edu>
2012-09-25 10:56:18 -0400

    Working on beginnings of the parser.

--- parser.rb
+++ parser.rb
@@ -1,7 +1,25 @@
 class Parse
   def initialize(text)
     @text = text
+    @sheet_data = {}
+    @results = []
   end
 
+  def scan_for_ruby
+    scan_results = @test.scan(/\<\%(=?)(.*?)-?\%\>/m)
+    scan_results.each do |result|
+      if result.length == 2
+        # check if there is an equal sign and print the output
+        if result[0] =~ /=/
+          @results = eval(result[1])
+        end
+      else
+ 
+      end
+    end
+  end
 
+  def perform_code
+
+  end
 end
