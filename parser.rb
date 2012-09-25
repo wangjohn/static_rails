891b85e19cf2525f6c125074bb9d4f97fec2dfc6
John J Wang <wangjohn@mit.edu>
2012-09-25 11:48:33 -0400

    Attempting to get some bindings to pass locals from eval.

--- parser.rb
+++ parser.rb
@@ -1,20 +1,20 @@
 class Parse
   def initialize(text)
-    @text = text
-    @sheet_data = {}
-    @results = []
+    @@text = text
+    @@sheet_data = {}
+    @@results = []
   end
 
   def scan_for_ruby
-    scan_results = @test.scan(/\<\%(=?)(.*?)-?\%\>/m)
+    scan_results = @@test.scan(/\<\%(=?)(.*?)-?\%\>/m)
     scan_results.each do |result|
       if result.length == 2
         # check if there is an equal sign and print the output
         if result[0] =~ /=/
-          @results = eval(result[1])
+          @@results = eval(result[1])
         end
       else
- 
+        eval(result[0]) 
       end
     end
   end
@@ -23,3 +23,33 @@ class Parse
 
   end
 end
+
+class BindingWrapper
+  def initialize
+  end 
+  def get_binding
+    return binding
+  end
+end
+
+class CodeEvaluation
+  def initialize(code)
+    @code = code
+    @binding = BindingWrapper.new
+    @variables_hash = {}
+  end
+
+  def evaluate
+    eval(@code, @binding.get_binding)
+    @variables = @binding.eval('local_variables')
+    puts @variables
+  end
+
+  def get_locals
+    @variables_hash
+  end
+end
+
+ce = CodeEvaluation.new("a = 5")
+ce.evaluate
+ce.get_locals
