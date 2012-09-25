47ee2a6a56b9eb294e0c157251c119c891ae5ff9
John J Wang <wangjohn@mit.edu>
2012-09-25 15:59:31 -0400

    Adding parameters for use in the erb file.

--- parser.rb
+++ parser.rb
@@ -1,26 +1,63 @@
 class Parse
-  def initialize(text)
-    @@text = text
-    @@sheet_data = {}
-    @@results = []
+
+  # regexes for the erb insertion
+  PARAMETER_REGEXES = {
+    :no_line_break => /-\%\>/,
+    :equals => /\<\%=/
+  }
+
+  def initialize(htmlfile, datafile)
+    @@binding_wrapper = BindingWrapper.new
+    @@text = File.new(htmlfile, 'r').read
+    inject_data(datafile)
   end
 
   def scan_for_ruby
-    scan_results = @@test.scan(/\<\%(=?)(.*?)-?\%\>/m)
+    results = []
+    scan_results = @@text.scan(/(\<\%=?)(.*?)(-?\%\>)/m)
     scan_results.each do |result|
-      if result.length == 2
-        # check if there is an equal sign and print the output
-        if result[0] =~ /=/
-          @@results = eval(result[1])
-        end
+      parameters = check_regex_results(result)
+      substitution_string = ""
+      if parameters[:equals]
+        # if there is an equal sign, then we must output the result
+        substitution_string = eval(result[1], @@binding_wrapper.get_binding).to_s
       else
-        eval(result[0]) 
+        # otherwise, we only evaluate the result
+        eval(result[0], @@binding_wrapper.get_binding) 
+      end
+      if parameters[:equals] && !parameters[:no_line_break]
+        # put in a line break only when we output results, and only when there's supposed to be a line break
+        substitution_string += "\n"
       end
     end
+    results
   end
 
   def perform_code
+    results = scan_for_ruby
+    new_text = @@text
+    counter = 0
+    while new_text =~ /\<\%=(.*?)-?\%\>/m
+    	new_text = new_text.gsub(/\<\%=(.*?)-?\%\>/m, results[counter])
+        counter += 1
+    end
+    return new_text
+  end
 
+  def inject_data(datafile)
+    file = File.new(datafile, 'r').read
+    eval(file, @@binding_wrapper.get_binding)
+  end
+
+  private 
+  def check_regex_results(result)
+    parameters = {}
+    result.each do |match|
+      PARAMETER_REGEXES.each do |key, value|
+        parameters[key] = true if match =~ value
+      end
+    end
+    parameters
   end
 end
 
@@ -32,24 +69,6 @@ class BindingWrapper
   end
 end
 
-class CodeEvaluation
-  def initialize(code)
-    @code = code
-    @binding = BindingWrapper.new
-    @variables_hash = {}
-  end
 
-  def evaluate
-    eval(@code, @binding.get_binding)
-    @variables = @binding.eval('local_variables')
-    puts @variables
-  end
-
-  def get_locals
-    @variables_hash
-  end
-end
-
-ce = CodeEvaluation.new("a = 5")
-ce.evaluate
-ce.get_locals
+parsed = Parse.new('test_html.html.erb', 'test_data.rb')
+puts parsed.perform_code
---
+++ test_data.rb
@@ -0,0 +1,1 @@
+@hash = ['wang', 'reddy', 'johnson']
