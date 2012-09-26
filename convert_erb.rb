f40830cc0624d4e3af3f40b332fbdb5c39f6bfdf
John J Wang <wangjohn@mit.edu>
2012-09-26 18:22:08 -0400

    Added a relative path ability.

--- convert_erb.rb
+++ convert_erb.rb
@@ -1,9 +1,9 @@
 require 'parser'
 
+RELATIVE_PATH = '/'
 FILES_TO_CONVERT = {
   'test_html.html.erb' => 'test_html.html'
 }
-
 DATA_FILES = [
   'test_data.rb'
 ]
@@ -11,12 +11,12 @@ DATA_FILES = [
 def merged_data_files
   output = ''
   for filename in DATA_FILES
-    output += File.open(filename, 'r').read
+    output += File.open(RELATIVE_PATH + filename, 'r').read
   end
   output
 end
 
 FILES_TO_CONVERT.each do |input_file, output_file|
-  parser = Parse.new(input_file, merged_data_files)
-  File.open(output_file, 'w'){|f| f.write(parser.perform_code)}
+  parser = Parse.new(RELATIVE_PATH + input_file, merged_data_files)
+  File.open(RELATIVE_PATH + output_file, 'w'){|f| f.write(parser.perform_code)}
 end
